a06dcd6d4d2eeda90180d4180f18dba3bfb40ef4
Lukas67 <l.gamp@hotmail.com>
2023-05-15 14:50:52 +0200

    UMAP explained

--- diff_exp_limma.R
+++ diff_exp_limma.R
@@ -25,34 +25,34 @@ evidence_data <- evidence_data[ , which(meta_data_0$ID == colnames(evidence_data
 # plot basic counts
 #barplot(log10(dim(evidence_data)[1]), main = "log Count of rows")
 
-# plot median intensities 
-evidence_data_medians <- data.frame(median_intensity = colMedians(as.matrix(evidence_data)))
-
-evidence_data_medians %>%
-  ggplot() +
-  aes(x = median_intensity, 
-      y = meta_data_0$Group,
-      fill = meta_data_0$Group) +
-  geom_boxplot() +
-  scale_x_log10() +
-  labs(fill=as.character(meta_data_0$Group), y=as.character(meta_data_0$Group))
+# # plot median intensities 
+# evidence_data_medians <- data.frame(median_intensity = colMedians(as.matrix(evidence_data)))
+# 
+# evidence_data_medians %>%
+#   ggplot() +
+#   aes(x = median_intensity, 
+#       y = meta_data_0$Group,
+#       fill = meta_data_0$Group) +
+#   geom_boxplot() +
+#   scale_x_log10() +
+#   labs(fill=as.character(meta_data_0$Group), y=as.character(meta_data_0$Group))
 
 # plot covariance
 #heatmap(cor(t(evidence_data)))
 
 # feature wise output across channels
-selectedProtein <- "P26640"
-to_plot <- data.frame(t(evidence_data[selectedProtein, ]))
-to_plot$names <- rownames(to_plot)
-
-library(reshape2)
-
-d = melt(to_plot, id.vars = "names")
-
-ggplot(data = d,
-       mapping = aes(x = names, y = value, fill=meta_data_0$Batch)) + 
-  geom_col(position = position_dodge()) +
-  labs(y=paste("intensity of", selectedProtein))
+# selectedProtein <- "P26640"
+# to_plot <- data.frame(t(evidence_data[selectedProtein, ]))
+# to_plot$names <- rownames(to_plot)
+# 
+# library(reshape2)
+# 
+# d = melt(to_plot, id.vars = "names")
+# 
+# ggplot(data = d,
+#        mapping = aes(x = names, y = value, fill=meta_data_0$Batch)) + 
+#   geom_col(position = position_dodge()) +
+#   labs(y=paste("intensity of", selectedProtein))
 
 # log transform expression values
 evidence_data <- log10(evidence_data) 
@@ -64,59 +64,59 @@ protein_matrix <- sweep(protein_matrix, 1, rowMeans(protein_matrix), FUN="/")
 evidence_data <- data.frame(protein_matrix)
 
 
-evidence_data <- replace(evidence_data, is.na(evidence_data), median(unlist(evidence_data), na.rm = TRUE))
+# evidence_data <- replace(evidence_data, is.na(evidence_data), median(unlist(evidence_data), na.rm = TRUE))
 
 
-# test MA plot and constand
-
-MAplot <- function(x,y,use.order=FALSE, R=NULL, cex=1.6, showavg=TRUE) {
-  # catch unequal size of matrices
-  if (dim(x)[2] != dim(y)[2]) {
-    if (dim(x)[2] > dim(y)[2]) {
-      x <- x[, 1:dim(y)[2]]
-    } else if (dim(y)[2] > dim(x)[2]) {
-      y <- y[, 1:dim(x)[2]]
-    }
-  }
-  
-  # make an MA plot of y vs. x that shows the rolling average,
-  M <- log2(y/x)
-  xlab = 'A'
-  if (!is.null(R)) {r <- R; xlab = "A (re-scaled)"} else r <- 1
-  A <- (log2(y/r)+log2(x/r))/2
-  if (use.order) {
-    orig.order <- order(A)
-    A <- orig.order
-    M <- M[orig.order]
-    xlab = "original rank of feature magnitude within IPS"
-  }
-  # select only finite values
-  use <- is.finite(M)
-  A <- A[use]
-  M <- M[use]
-  # plot
-  print(var(M))
-  plot(A, M, xlab=xlab, cex.lab=cex, cex.axis=cex)
-  # rolling average
-  if (showavg) { lines(lowess(M~A), col='red', lwd=5) }
-}
-
-exp_matrix_0 <- as.matrix(evidence_data)
-colnames(exp_matrix_0) <- meta_data_0$Group
-
-user_choice <- "EC-HC"
-# split user choice of comp back to sample types
-user_choice_vector <- strsplit(user_choice, split = "-")
-# and assign them to a variable
-choice_A <- user_choice_vector[[1]][1]
-choice_B <- user_choice_vector[[1]][2]
-
-#find the row indeces of corresponding to the individual sample types
-st_indeces <- split(seq_along(meta_data_0$Group), meta_data_0$Group)
-index_A <- st_indeces[choice_A]
-index_B <- st_indeces[choice_B]
-
-MAplot(exp_matrix_0[,index_A[[1]]], exp_matrix_0[,index_B[[1]]])
+# # test MA plot and constand
+# 
+# MAplot <- function(x,y,use.order=FALSE, R=NULL, cex=1.6, showavg=TRUE) {
+#   # catch unequal size of matrices
+#   if (dim(x)[2] != dim(y)[2]) {
+#     if (dim(x)[2] > dim(y)[2]) {
+#       x <- x[, 1:dim(y)[2]]
+#     } else if (dim(y)[2] > dim(x)[2]) {
+#       y <- y[, 1:dim(x)[2]]
+#     }
+#   }
+#   
+#   # make an MA plot of y vs. x that shows the rolling average,
+#   M <- log2(y/x)
+#   xlab = 'A'
+#   if (!is.null(R)) {r <- R; xlab = "A (re-scaled)"} else r <- 1
+#   A <- (log2(y/r)+log2(x/r))/2
+#   if (use.order) {
+#     orig.order <- order(A)
+#     A <- orig.order
+#     M <- M[orig.order]
+#     xlab = "original rank of feature magnitude within IPS"
+#   }
+#   # select only finite values
+#   use <- is.finite(M)
+#   A <- A[use]
+#   M <- M[use]
+#   # plot
+#   print(var(M))
+#   plot(A, M, xlab=xlab, cex.lab=cex, cex.axis=cex)
+#   # rolling average
+#   if (showavg) { lines(lowess(M~A), col='red', lwd=5) }
+# }
+# 
+# exp_matrix_0 <- as.matrix(evidence_data)
+# colnames(exp_matrix_0) <- meta_data_0$Group
+# 
+# user_choice <- "EC-HC"
+# # split user choice of comp back to sample types
+# user_choice_vector <- strsplit(user_choice, split = "-")
+# # and assign them to a variable
+# choice_A <- user_choice_vector[[1]][1]
+# choice_B <- user_choice_vector[[1]][2]
+# 
+# #find the row indeces of corresponding to the individual sample types
+# st_indeces <- split(seq_along(meta_data_0$Group), meta_data_0$Group)
+# index_A <- st_indeces[choice_A]
+# index_B <- st_indeces[choice_B]
+# 
+# MAplot(exp_matrix_0[,index_A[[1]]], exp_matrix_0[,index_B[[1]]])
 
 
 
@@ -154,61 +154,63 @@ fviz_pca_ind(data.pca, col.ind = "cos2",
              repel = TRUE # Avoid text overlapping (slow if many points)
 )
 
-# batch effects obtained --> use combat to correct
-batch <- meta_data_0$Batch
 
-library("scater")
-dimred_pca <- calculatePCA(evidence_data,                                          ncomponents = 5,
-                           ntop = Inf,
-                           scale = TRUE)
-
-dimred_pca <- data.frame(dimred_pca)
-
-# sample continuos variable to test 
-
-meta_data_0$body_weight <- rnorm(nrow(meta_data_0), mean=65)
-
-ggplot(dimred_pca, aes(x=PC1, 
-                       y=PC2, 
-                       color=meta_data_0$Batch,
-                       shape = meta_data_0$Gender,
-                       size = meta_data_0$body_weight)) +
-  geom_point(alpha=3/4)
-
-
-dimred_umap <- calculateUMAP(evidence_data,
-                             ncomponents = 5,
-                             ntop = Inf,
-                             scale = TRUE)
-
-dimred_umap <- data.frame(dimred_umap)
-
-ggplot(dimred_umap, aes(x=UMAP1, 
-                        y=UMAP2, 
-                        color=meta_data_0$Batch,
-                        shape = meta_data_0$Gender,
-                        size = meta_data_0$body_weight)) +
-  geom_point(alpha=3/4)
-
-plot_ly(x=dimred_umap$UMAP1,
-        y=dimred_umap$UMAP2, 
-        z=dimred_umap$UMAP3, 
-        type="scatter3d", 
-        mode="markers",
-        color=meta_data_0$Batch) 
+# library("scater")
+# dimred_pca <- calculatePCA(evidence_data,                                          ncomponents = 5,
+#                            ntop = Inf,
+#                            scale = TRUE)
+# 
+# dimred_pca <- data.frame(dimred_pca)
+# 
+# # sample continuos variable to test 
+# 
+# meta_data_0$body_weight <- rnorm(nrow(meta_data_0), mean=65)
+# 
+# ggplot(dimred_pca, aes(x=PC1, 
+#                        y=PC2, 
+#                        color=meta_data_0$Batch,
+#                        shape = meta_data_0$Gender,
+#                        size = meta_data_0$body_weight)) +
+#   geom_point(alpha=3/4)
+# 
+# 
+# dimred_umap <- calculateUMAP(evidence_data,
+#                              ncomponents = 5,
+#                              ntop = Inf,
+#                              scale = TRUE)
+# 
+# dimred_umap <- data.frame(dimred_umap)
+# 
+# ggplot(dimred_umap, aes(x=UMAP1, 
+#                         y=UMAP2, 
+#                         color=meta_data_0$Batch,
+#                         shape = meta_data_0$Gender,
+#                         size = meta_data_0$body_weight)) +
+#   geom_point(alpha=3/4)
+# 
+# plot_ly(x=dimred_umap$UMAP1,
+#         y=dimred_umap$UMAP2, 
+#         z=dimred_umap$UMAP3, 
+#         type="scatter3d", 
+#         mode="markers",
+#         color=meta_data_0$Batch) 
 
 
 library(sva)
 
-combat_edata1 = ComBat(dat=evidence_data_norm, batch=batch)
+# batch effects obtained --> use combat to correct
+batch <- meta_data_0$Batch
+
+
+combat_edata1 = ComBat(dat=evidence_data, batch=batch)
 
 # or use limma
-y2 <- removeBatchEffect(evidence_data_norm, batch)
+y2 <- removeBatchEffect(evidence_data, batch)
 
 # compare corrected batch effects
-boxplot(as.data.frame(evidence_data_norm),main="Original", ylim=c(0.01,0.05))
-boxplot(as.data.frame(y2),main="Batch corrected with Limma", ylim=c(0.01,0.05))
-boxplot(as.data.frame(combat_edata1),main="Batch corrected with Combat", ylim=c(0.01,0.05))
+boxplot(as.data.frame(evidence_data),main="Original")
+boxplot(as.data.frame(y2),main="Batch corrected with Limma")
+boxplot(as.data.frame(combat_edata1),main="Batch corrected with Combat")
 
 # Perform differential expression analysis between each of the two groups
 
